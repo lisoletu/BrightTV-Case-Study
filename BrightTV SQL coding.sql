d04a131cf17f3ae5b713337ad95b47d35dc0a7b3
lisoletu <lisoletudigala@gmail.com>
2026-07-09 15:31:10 +0000

    Gender checks

--- BrightTV SQL coding.sql
+++ BrightTV SQL coding.sql
@@ -1,8 +1,33 @@
 -- Databricks notebook source
-SELECT*
-FROM brighttv.analytics.user_profiles;
-
-SELECT *
-FROM brighttv.analytics.viewership;
+-------------------------------------------------------------------------
+---Telling Databricks to use the "brighttv" catalog and "analytics" schema
+-------------------------------------------------------------------------
 
 USE brighttv.analytics;
+
+-------------------------------------------------------
+---Running the full tables---
+-------------------------------------------------------
+
+SELECT*
+FROM user_profiles;
+
+SELECT *
+FROM viewership;
+
+-------------------------------------------------
+---GENDER CHECKS
+--------------------------------------------------
+SELECT DISTINCT Gender
+FROM user_profiles;
+
+SELECT DISTINCT
+    CASE
+        WHEN Gender ='None' THEN 'Unknown' --Replaces the value "None" with "unknown"
+        WHEN Gender =' ' THEN 'Unknown'--Replaces the empty space with "unknown"
+    ELSE Gender -- if gender is male or female turn it as it is
+    END AS Sex --new column name
+FROM user_profiles;
+
+
+
