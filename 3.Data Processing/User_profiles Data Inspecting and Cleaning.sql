e14f98280d8b3186358f422819ae95f8a6e9b485
lisoletu <lisoletudigala@gmail.com>
2026-08-01 18:21:50 +0000

    updating changes

--- 3.Data Processing/User_profiles Data Inspecting and Cleaning.sql
+++ 3.Data Processing/User_profiles Data Inspecting and Cleaning.sql
@@ -1,4 +1,170 @@
 -- Databricks notebook source
+
+-- This is to check what my data looks like.
 SELECT *
+FROM brighttv.analytics.user_profiles
+LIMIT 10;
+--------------------------------------------
+-- Checking for Duplicates
+--------------------------------------------
+SELECT COUNT(*),
+       userid
+FROM brighttv.analytics.user_profiles
+GROUP BY userid
+HAVING COUNT(*)>1;
+
+----------------------------------------------------------
+---EMAIL CHECKS
+----------------------------------------------------------
+SELECT DISTINCT Email
 FROM brighttv.analytics.user_profiles;
+
+SELECT DISTINCT UserID, Email,
+    CASE
+        WHEN Email IS NOT NULL 
+            OR Email <>' ' 
+            OR Email NOT IN ('None') THEN 1
+        ELSE 0
+        END AS email_flag
+FROM brighttv.analytics.user_profiles;
+
+----------------------------------------------------------
+---GENDER CHECKS
+----------------------------------------------------------
+SELECT DISTINCT Gender
+FROM user_profiles;
+
+SELECT DISTINCT
+    CASE
+        WHEN Gender ='None' THEN 'Unknown' --Replaces the value None with unknown
+        WHEN Gender =' ' THEN 'Unknown'--Replaces the empty space with unknown
+        WHEN Gender IS NULL THEN 'unkonwn' --Replaces the null with unknown
+    ELSE Gender -- if gender is male or female turn it as it is
+    END AS Sex --new column name
+FROM brighttv.analytics.user_profiles;
+-----------------------------------------------------------------
+-----RACE CHECKS
+-----------------------------------------------------------------
+SELECT DISTINCT Race
+FROM brighttv.analytics.user_profiles;
+
+SELECT COUNT (DISTINCT UserID) AS Subs,
+    CASE
+        WHEN Race ='None' THEN 'Unknown' ----Replaces None with unknown
+        WHEN Race ='other' THEN 'Unknown'-----Replaces other with unknown
+        WHEN Race =' ' THEN 'Unknown'-----Replaces empty space with unknown
+        WHEN Race IS NULL THEN 'Unknown'-----Replaces null with unknown
+    ELSE Race --keep it as it is
+   END AS ethnicity -- new column name
+   FROM brighttv.analytics.user_profiles
+GROUP BY ethnicity;
+
+------------------------------------------------------------------
+---AGE CHECKS
+------------------------------------------------------------------
+SELECT DISTINCT MIN(Age) AS min_age, -- 0  (to find age of the youngest person)
+                MAX(Age) AS max_age, -- 114  (to find age of the oldest person)
+                AVG (Age) AS mean_age-- 27.696  (to find average age between upper bound and lower bound)
+FROM brighttv.analytics.user_profiles;
+
+SELECT 
+    CASE
+         WHEN Age = 0 THEN 'Infant'
+        WHEN Age BETWEEN 1 AND 12 THEN 'Kids'
+        WHEN Age BETWEEN 13 AND 17 THEN 'Youth'
+        WHEN Age BETWEEN 18 AND 35 THEN 'Young Adult'
+        WHEN Age BETWEEN 36 AND 50 THEN 'Adults'
+        WHEN Age > 50 AND AGE<=60 THEN 'Elder' --Another way of doing a BETWEEN statement using operations
+        WHEN Age > 60 THEN 'Pensioner'
+    END AS Age_group
+FROM brighttv.analytics.user_profiles;
+
+
+---------------------------------------------------------------------
+---PROVINCE CHECKS
+--------------------------------------------------------------------
+SELECT DISTINCT Province
+FROM brighttv.analytics.user_profiles;
+
+SELECT DISTINCT
+CASE
+        WHEN Province ='None' THEN 'Unknown'
+        WHEN Province =' ' THEN 'Unknown'
+        WHEN Province IS NULL THEN 'Unknown'
+    ELSE Province
+    END AS Region
+FROM brighttv.analytics.user_profiles;
+
+---------------------------------------------------------------------
+---SOCIAL MEDIA HANDLE CHECKS
+--------------------------------------------------------------------
+SELECT DISTINCT UserID, `Social Media Handle`
+FROM brighttv.analytics.user_profiles;
+
+SELECT DISTINCT UserID, `Social Media Handle`,
+ CASE
+        WHEN `Social Media Handle` IS NOT NULL 
+        AND `Social Media Handle`<>' ' 
+        AND  `Social Media Handle` <> 'None'
+    THEN 1
+    ELSE 0
+    END AS sm_flag
+FROM brighttv.analytics.user_profiles;
+
+
+----------------------------------------------------------------------
+---Creating clean user_profiles table using TEMPORARY TABLES
+----------------------------------------------------------------------
+CREATE OR REPLACE TEMPORARY TABLE user_profiles AS (
+
+    SELECT UserID,
+
+    CASE 
+        WHEN Province=' ' THEN 'Uncategorized'
+        WHEN Province='None' THEN 'Uncategorized'
+        WHEN Province ='other' THEN 'Uncategorized'
+        WHEN Province IS NULL THEN 'Uncategorized'
+    ELSE Province
+    END AS Region,
+
+    age,
+    CASE
+        WHEN age = 0 THEN 'Infants:0'
+        WHEN age BETWEEN 1 AND 12 THEN 'Kids:1-12'
+        WHEN age BETWEEN 13 AND 19 THEN 'Teenager:13-19'
+        WHEN age BETWEEN 20 AND 35 THEN 'Youth:20-35'
+        WHEN age BETWEEN 36 AND 50 THEN 'Adult:36-50'
+        WHEN age BETWEEN 51 AND 65 THEN 'Elder:51-65'
+        WHEN age >65 THEN 'Pensioner:>65'
+    END AS age_groups,
+
+    CASE
+        WHEN (email IS NOT NULL )OR (email=' ') OR  (email NOT IN ('None'))THEN 1
+    ELSE 0
+    END AS email_flag,
+
+    CASE
+        WHEN `Social Media Handle` IS NOT NULL OR `Social Media Handle`=' ' OR  `Social Media Handle` NOT IN ('None')THEN 1
+        ELSE 0
+    END AS sm_flag,
+
+    CASE
+        WHEN Race='other' THEN 'None'
+        WHEN Race=' ' THEN 'None'
+    ELSE Race
+    END AS Race,
+
+    CASE
+        WHEN gender =' ' THEN 'None'
+        ELSE gender
+    END AS Gender
+
+FROM brighttv.analytics.user_profiles
+);
+
+---------------------------------------------------------------------------------
+---to see what is on my TEMP table
+------------------------------------------------------------------------------------
+SELECT *
+FROM user_profiles;
 
--- 3.Data Processing/Viewership Data Inspection and Cleaning.sql
+++ 3.Data Processing/Viewership Data Inspection and Cleaning.sql
@@ -1,3 +1,53 @@
 -- Databricks notebook source
+--- I want to see what is in this ORIGINAL viewership table/see the structure of the data
 SELECT *
+FROM brighttv.analytics.viewership
+LIMIT 10;
+
+
+--Applying DATE FUNCTIONS to extracting watch time in YY-MM-DD format from 'RecordDate2' column (timestamp) into Date
+SELECT  RecordDate2,
+        TO_DATE(RecordDate2) AS watch_date --TO_DATE function converts a string (timestamp) into a date (YYYY-MM-DD)
 FROM brighttv.analytics.viewership;
+
+
+---Using more DATE FUNCTIONS to extract dates (year, month, day) 
+SELECT
+    UserID0,
+    RecordDate2,
+    TO_DATE(RecordDate2) AS watch_date, --TO_DATE Converts a string into a date YYYY-MM-DD
+    DAYNAME(TO_DATE(RecordDate2)) AS day_name, -- Extracts the day name (Mon-Sun)
+    MONTHNAME(TO_DATE(RecordDate2)) AS month_name, -- Extracts the month name (Jan-Dec)
+    YEAR(TO_DATE(RecordDate2)) AS event_year, -- Extracts the year value
+    DAY(TO_DATE(RecordDate2)) AS event_dt -- Extracts day value (1-31)
+FROM brighttv.analytics.viewership;
+
+
+---Creating TEMPORARY table called 'clean_viwership' with clean/processed vieweriship result- to help us analyze veiwing patterns/trends
+CREATE OR REPLACE TEMPORARY TABLE clean_viewership AS (
+SELECT 
+    COUNT(DISTINCT userID0) AS number_of_subs,--counting number of subscribers
+    RecordDate2,
+    TO_DATE (RecordDate2) AS watch_date, --converts a string into a date YYYY-MM-DD
+    DAYNAME(TO_DATE (RecordDate2)) AS day_name, -- extract the day of the week name
+        CASE 
+            WHEN DAYNAME(TO_DATE (RecordDate2))  IN ('Sat', 'Sun') THEN '02.Weekend'
+            ELSE '01.Weekday'
+        END AS Day_classification,
+    MONTHNAME(TO_DATE (RecordDate2)) AS month_name, --extracts the month name
+    YEAR(TO_DATE (RecordDate2)) AS event_year,-- extracts the year
+    DAY(TO_DATE (RecordDate2)) AS event_dt-- extracts the day value
+    FROM brighttv.analytics.viewership
+    WHERE userID0 IS NOT NULL
+    GROUP BY ALL
+    ORDER BY watch_date DESC);
+
+----This shows me the output of the NEW viewership table (temp table-clean_viewership) created from the code above NOT the original viewership table
+SELECT *
+FROM clean_viewership;
+
+--How many subs are watching weekdays and weekends
+SELECT SUM(number_of_subs) AS subs,
+       Day_classification
+FROM clean_viewership
+GROUP BY Day_classification;
